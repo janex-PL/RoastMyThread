b72ca4c371124435057175c00e1e27a6acf80dec
janex-PL <kliszczjan@gmail.com>
2024-09-02 22:27:07 +0200

    Initialize repo

---
+++ RoastMyThread.Web/Options/ThreadsConfiguration.cs
@@ -0,0 +1,8 @@
+public class ThreadsConfiguration
+{
+    public const string Key = "Threads";
+    
+    public string? AppId { get; set; }
+    public string? AppSecret { get; set; }
+    public string? RedirectUri { get; set; }
+}
---
+++ RoastMyThread.Web/Program.cs
@@ -0,0 +1,29 @@
+using MudBlazor.Services;
+using RoastMyThread.Web.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add MudBlazor services
+builder.Services.AddMudServices();
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.Configure<ThreadsConfiguration>(builder.Configuration.GetSection(ThreadsConfiguration.Key));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
